585070df662dafd4e12a0bb54e171ba770faf81f
Sri <sri.vasamsetti22@gmail.com>
2015-04-13 04:54:30 +0000

    ading things

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ var allowCrossDomain = function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods',"GET, POST, PUT, OPTIONS, DELETE");
-  res.header('Content-Type','application/x-www-form-urlencoded');
+  res.header('Content-Type','application/x-www-form-urlencoded', 'application/json');
   next();
 };
 app.use(allowCrossDomain);
